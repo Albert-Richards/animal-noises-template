1c66d9d22f33a36470f8400e78c5ae396971d818
Albert <albert.richards3@gmail.com>
2021-07-07 16:55:56 +0000

    database functionality added

--- server/app.py
+++ server/app.py
@@ -10,18 +10,18 @@ db = SQLAlchemy(app)
 class Animals(db.Model):
 	entry_no = db.Column(db.Integer, primary_key=True)
 	animal_entry = db.Column(db.String(50), nullable=False)
-    def __repr__(self):
-		return ''.join([str(self.animal_entry)])
+
+db.create_all()
 
 @app.route('/')
 def home():
     animal = requests.get('http://animal_api:5000/get_animal')
     noise = requests.post('http://animal_api:5000/get_noise', data=animal.text)
     response = f"The {animal.text} goes {noise.text}"
-    db.seesion.add(Animals(animal_entry=response)) 
+    db.session.add(Animals(animal_entry=response)) 
     db.session.commit()
     history = Animals.query.order_by(Animals.entry_no.desc()).limit(5).all()
-    return render_template('index.html', animal=animal.text, noise=noise.text)
+    return render_template('index.html', history=history, animal=animal.text, noise=noise.text)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
